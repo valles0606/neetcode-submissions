d484e8f42f20aab15dfd64488ba82c352da36e55
Andrew Valles <andrewvallescsw@gmail.com>
2026-09-09 22:15:01 -0700

    Add: validate-parentheses - submission-29

---
+++ Data Structures & Algorithms/validate-parentheses/submission-29.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Map<Character, Character> par = new HashMap<>();
+        par.put('[' , ']');
+        par.put('(' , ')');
+        par.put('{' , '}');
+
+        Stack<Character> stringStack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (par.containsKey(c)) {
+                stringStack.push(par.get(c));
+            } else if (!stringStack.isEmpty() && stringStack.peek() == c) {
+                stringStack.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return stringStack.isEmpty();
+    }
+}
