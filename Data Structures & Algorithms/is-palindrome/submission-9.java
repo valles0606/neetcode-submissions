9a2dce530f3c470d89677efc4610d19fadea8010
Andrew Valles <andrewvallescsw@gmail.com>
2026-09-10 10:08:08 -0700

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !isAlphaNum(s.charAt(left))) {
+                left++;
+            }
+
+            while (right > left && !isAlphaNum(s.charAt(right))) {
+                right--;
+            }
+            
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    private boolean isAlphaNum(char c) {
+        if (c >= 'A' && c <= 'Z') {return true;}
+        if (c >= 'a' && c <= 'z') {return true;}
+        if (c >= '0' && c <= '9') {return true;}
+
+        return false;
+    }
+}
